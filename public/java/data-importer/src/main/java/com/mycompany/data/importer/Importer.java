48e88b47eebcba024c173418836e84b2eaef40a6
phoomperfectionist <t.p.pungchaichan@gmail.com>
2016-02-17 21:35:29 +0700

    Add Data Importer for importing data to neo4j

---
+++ public/java/data-importer/src/main/java/com/mycompany/data/importer/Importer.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.data.importer;
+
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.neo4j.graphdb.DynamicRelationshipType;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+/**
+ *
+ * @author pperfectionist
+ */
+public class Importer {
+
+    public static final DynamicRelationshipType LINK = DynamicRelationshipType.withName("Call");
+
+    public static String toDate(String datetime) {
+
+        return datetime.substring(0, 4) + datetime.substring(5, 7) + datetime.substring(8, 10);
+    }
+
+    public static String toTome(String datetime) {
+        return datetime.substring(11, 13) + datetime.substring(14, 16) + datetime.substring(17, 19);
+    }
+
+    public static void main(String args[]) throws IOException {
+        Map<String, Integer> incoming = new HashMap<>();
+        Map<String, Integer> outgoing = new HashMap<>();
+        try (CSVReader reader = new CSVReader(new FileReader("large_sample.csv"), ',')) {
+            String[] nextLine;
+            
+            while ((nextLine = reader.readNext()) != null) {
+                if (incoming.containsKey(nextLine[1])) {
+                    incoming.put(nextLine[1], incoming.get(nextLine[1]) + 1);
+                } else {
+                    incoming.put(nextLine[1], 1);
+                }
+                
+                if (outgoing.containsKey(nextLine[0])) {
+                    outgoing.put(nextLine[0], outgoing.get(nextLine[0]) + 1);
+                } else {
+                    outgoing.put(nextLine[0], 1);
+                }
+            }
+            
+            reader.close();
+        } 
+        
+        GraphDatabaseService gdb = new GraphDatabaseFactory().newEmbeddedDatabase("/Users/pperfectionist/Documents/Neo4j/test.graphdb");
+
+        NodeLabel[] nl = NodeLabel.values();
+        int i;
+        long time;
+        Transaction tx;
+        try (CSVReader reader = new CSVReader(new FileReader("large_sample.csv"), ',')) {
+            String[] nextLine;
+            i = 0;
+            time = System.currentTimeMillis();
+            tx = gdb.beginTx();
+            Map<String, Node> nodes = new HashMap<>();
+            reader.readNext();
+            while ((nextLine = reader.readNext()) != null) {
+                Node a, b;
+                if (nodes.containsKey(nextLine[0])) {
+                    a = nodes.get(nextLine[0]);
+                } else {
+                    a = gdb.createNode(nl[0]);
+                    a.setProperty("number", nextLine[0]);
+                    a.setProperty("incoming", incoming.get(nextLine[0]) == null? 0 : incoming.get(nextLine[0]));
+                    a.setProperty("outgoing", outgoing.get(nextLine[0]) == null? 0 : outgoing.get(nextLine[0]));
+                    nodes.put(nextLine[0], a);
+                }
+
+                if (nodes.containsKey(nextLine[1])) {
+                    b = nodes.get(nextLine[1]);
+                } else {
+                    b = gdb.createNode(nl[0]);
+                    b.setProperty("number", nextLine[1]);
+                    b.setProperty("incoming", incoming.get(nextLine[1]) == null? 0 : incoming.get(nextLine[1]));
+                    b.setProperty("outgoing", outgoing.get(nextLine[1]) == null? 0 : outgoing.get(nextLine[1]));
+                    nodes.put(nextLine[1], b);
+                }
+
+                Relationship r = a.createRelationshipTo(b, LINK);
+//                r.setProperty("startTime", nextLine[1]);
+//                r.setProperty("startDate", nextLine[1]);
+//                r.setProperty("duration", nextLine[5]);
+//                r.setProperty("callDay", "");
+                r.setProperty("startTime", "");
+                r.setProperty("startDate", "");
+                r.setProperty("duration", "");
+                r.setProperty("callDay", "");
+                if (i % 50000 == 0) {
+                    tx.success();
+                    tx.finish();
+                    tx = gdb.beginTx();
+                }
+                i++;
+            }
+        }
+
+        tx.success();
+        tx.finish();
+        time = System.currentTimeMillis() - time;
+        System.out.println("import of " + i + " relationships took " + time / 1000 + " seconds.");
+        gdb.shutdown();
+    }
+}
---
+++ public/java/data-importer/src/main/java/com/mycompany/data/importer/NodeLabel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.data.importer;
+import org.neo4j.graphdb.Label;
+/**
+ *
+ * @author pperfectionist
+ */
+public enum NodeLabel implements Label {
+    Data,Data1, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9, Data10, Data11, Data12, Data13;
+}
