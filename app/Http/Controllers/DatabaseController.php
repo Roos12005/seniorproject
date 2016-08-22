c5399fa2a5fd8549045961b4baa06e84bd67cead
Pohfy123 <pohfy123@gmail.com>
2016-08-23 00:06:43 +0700

    remove Xmx6G

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -61,7 +61,8 @@ class DatabaseController extends Controller{
         try {
             $isGranted = $neo->grantLock($db_name);
             if($isGranted) {
-                $command = "java -Xmx6G -XX:+CMSClassUnloadingEnabled -jar java/data-importer/target/data-importer-1.0-SNAPSHOT.jar " . $db_name . ' ' . $feature_extraction . ' 2>&1';
+                $command = "java -XX:+CMSClassUnloadingEnabled -jar java/data-importer/target/data-importer-1.0-SNAPSHOT.jar " . $db_name . ' ' . $feature_extraction . ' 2>&1';
+//                $command = "java -Xmx6G -XX:+CMSClassUnloadingEnabled -jar java/data-importer/target/data-importer-1.0-SNAPSHOT.jar " . $db_name . ' ' . $feature_extraction . ' 2>&1';
                 $output = shell_exec($command);
                 Log::info($command);
                 Log::info($output);
